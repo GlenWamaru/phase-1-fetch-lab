97e651812bbce80329042fd67492b7b51a8220f8
Glen Wamaru <garpirungu@gmail.com>
2025-04-27 18:20:27 +0300

    Completed

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  // Don't forget to return fetch!
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      renderBooks(data); // <-- Call renderBooks here
+    });
 }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
-  books.forEach(book => {
+  books.forEach(function (book) {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
